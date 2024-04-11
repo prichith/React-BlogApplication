e2873afcc909599301158fe8eec4834188ccacaa
prichith cp <prichith@gmail.com>
2024-04-11 16:54:52 +0530

    first commit

---
+++ blog/src/Data.js
@@ -0,0 +1,57 @@
+// Articles
+const Data = [
+  {
+    heading: "TITLE HEADING",
+    date: "April 7, 2014",
+    description:
+      "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+    noOfComments: 0,
+    imageUrl: "https://www.w3schools.com/w3images/woods.jpg",
+  },
+  {
+    heading: "BLOG ENTRY",
+    date: "April 2, 2014",
+    description:
+      "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+    noOfComments: 2,
+    imageUrl: "https://www.w3schools.com/w3images/bridge.jpg",
+  },
+];
+
+// MyName About blogger
+const AboutMe = {
+  imageUrl: "https://www.w3schools.com/w3images/avatar_g.jpg",
+  aboutMe:
+    "Just me, myself and I, exploring the universe of uknownment. I have a heart of love and a interest of lorem ipsum and mauris neque quam blog. I want to share my world with you.",
+};
+
+//popular posts data
+const PopularPostsList = ([
+    {
+        heading :"Lorem",
+        title :"Sed mattis nunc",
+        imageUrl : "https://www.w3schools.com/w3images/workshop.jpg"
+    },
+    {
+        heading :"Ipsum",
+        title :"Praes tinci sed",
+        imageUrl : "https://www.w3schools.com/w3images/gondol.jpg"
+    },
+    {
+        heading :"Dorum",
+        title :"Ultricies congue",
+        imageUrl : "https://www.w3schools.com/w3images/skies.jpg"
+    },
+    {
+        heading :"Mingsum",
+        title :"Mingsum",
+        imageUrl : "https://www.w3schools.com/w3images/rock.jpg"
+    }
+])
+
+// tags data
+const TagsData = (
+    ["Travel","New York","London","IKEA","NORWAY","DIY","Ideas","Baby","Family","News","Clothing","Shopping","Sports","Games"]
+)
+
+export { Data, AboutMe, PopularPostsList , TagsData };
